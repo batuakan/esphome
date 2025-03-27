c291a7b990cb3db0e0213f4bab03b36b830b86ef
Batu Akan <batu.akan@gmail.com>
2025-03-27 23:45:53 +0100

    Added support for espidf framework

--- esphome/components/signalk/signalk.h
+++ esphome/components/signalk/signalk.h
@@ -4,7 +4,17 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 
+// #include "WiFiClientSecure.h"
+// #include "WebSocketsClient.h"
+
+#ifdef USE_ESP32_FRAMEWORK_ARDUINO
 #include <ArduinoWebsockets.h>
+#endif
+
+#if defined(USE_ESP_IDF)
+#include "esp_websocket_client.h"
+#endif
+
 #include "signalk_sensor_base.h"
 
 namespace esphome {
@@ -22,9 +32,10 @@ class SignalK : public PollingComponent {
     sensors_.insert(std::pair<std::string, SignalkSensorBase *>(sensor->get_path(), sensor));
   }
 
- protected:
-  void connect();
+  void on_connected();
+  void on_receive_delta(uint8_t *payload, size_t length);
 
+ protected:
   unsigned long last_millis;
   unsigned long elapsed_duration;
   bool connected;
@@ -32,7 +43,12 @@ class SignalK : public PollingComponent {
   std::string host_;
   unsigned short port_;
   std::map<std::string, SignalkSensorBase *> sensors_;
+#ifdef USE_ESP32_FRAMEWORK_ARDUINO
   websockets::WebsocketsClient webSocketClient_;
+#endif
+#if defined(USE_ESP_IDF)
+  esp_websocket_client_handle_t websocket_client;
+#endif
 };
 
 }  // namespace signalk
